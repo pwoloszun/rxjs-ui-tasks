1c039fd3c2ad7d052cfbdaf667ce0793eb14448b
pwoloszun <piotr.woloszun@gmail.com>
2022-11-03 14:03:56 +0100

    mysearch minors

--- src/app/features/flattening-operators/components/my-search/my-search.component.ts
+++ src/app/features/flattening-operators/components/my-search/my-search.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 import { SearchApiService } from '@api/search-api.service';
+import { liveSearch } from '@shared/operators/live-search';
 import { filter, map, of, switchMap, debounceTime } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
 
@@ -18,16 +19,22 @@ export class MySearchComponent {
 
   // TODO
   // min length: MIN_SEARCH_QUERY_LENGTH
-  // throttle/debounce 1200
+  // throttle/debounce 600
   // ignore if query did not change:
   // send req
   // handle ONLY latest req + cancel previous req
   searchResults$ = this.searchTextCtrl.valueChanges.pipe(
     map((query) => query === null ? '' : query),
-    debounceTime(1200),
+    debounceTime(600),
     filter((q) => q.length > MIN_SEARCH_QUERY_LENGTH),
     distinctUntilChanged(),
     switchMap((query: string) => this.searchApiService.querySearch$(query))
+  );
+
+
+  searchResults$_TODO = this.searchTextCtrl.valueChanges.pipe(
+    map((query) => query === null ? '' : query),
+    liveSearch(600, (q) => this.searchApiService.querySearch$(q))
   );
 
   constructor(private searchApiService: SearchApiService) { }
