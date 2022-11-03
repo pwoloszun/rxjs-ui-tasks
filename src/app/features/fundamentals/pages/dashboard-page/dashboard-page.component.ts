74f59ae3adc0c9e5085bb9ff6eac757c65b31d1d
pwoloszun <piotr.woloszun@gmail.com>
2022-11-03 12:25:41 +0100

    async exmpl

--- src/app/features/fundamentals/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/features/fundamentals/pages/dashboard-page/dashboard-page.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { delay, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
   title = 'Dashboard';
+
+  person$ = this.getPerson$();
 
   constructor() { }
 
   ngOnInit(): void {
+    // TODO
+    // setTimeout(() => {
+    //   console.log('Later subscription');
+    // }, 4000);
+  }
+
+  getPerson$() {
+    return of({ name: 'bob', age: 123 }).pipe(
+      tap(() => console.log('REQUEST fetch person')),
+      delay(2000),
+      tap(() => console.log('RESPONSE fetch person')),
+    );
+  }
+
+  ngOnDestroy(): void {
+    // cleanup
   }
 
 }
