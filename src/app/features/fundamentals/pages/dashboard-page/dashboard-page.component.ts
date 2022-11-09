d3c84b2f6d895673697c2583e017ab664ad43bf8
pwoloszun <piotr.woloszun@gmail.com>
2022-11-09 13:11:44 +0100

    async, ngrLEt share exmpls

--- src/app/features/fundamentals/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/features/fundamentals/pages/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { PersonService } from '@features/fundamentals/services/person.service';
+import { share, Subscription, Observable, shareReplay } from 'rxjs';
+import { fullObserver } from '../../../../utils/full-observer';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -8,12 +10,34 @@ import { PersonService } from '@features/fundamentals/services/person.service';
 })
 export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  person$ = this.personService.getPerson$();
+  person$ = this.personService.getPerson$().pipe(
+    // share()
+    shareReplay()
+  );
+
+  // person$!: Observable<any>;
+
+  // personData!: any;
+
+  // private subscriptionsCon = new Subscription();
 
   constructor(private personService: PersonService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   console.log('LATER:',);
+    //   this.person$.subscribe(fullObserver('LATER'));
+    // }, 4000);
 
-  ngOnDestroy(): void { }
+    // this.person$ = this.personService.getPerson$();
+    // const sub = this.person$.subscribe((data) => {
+    //   this.personData = data;
+    // });
+    // this.subscriptionsCon.add(sub);
+  }
+
+  ngOnDestroy(): void {
+    // this.subscriptionsCon.unsubscribe();
+  }
 
 }
