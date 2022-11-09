de8a01ff2b614b55b6ef983f6ad4330715dc493b
pwoloszun <piotr.woloszun@gmail.com>
2022-11-09 16:52:55 +0100

    cleanup

--- src/app/features/combine-streams/pages/lazy-forex-page/lazy-forex-page.component.ts
+++ src/app/features/combine-streams/pages/lazy-forex-page/lazy-forex-page.component.ts
@@ -33,24 +33,17 @@ export class LazyForexPageComponent implements OnInit {
   updateBtnClick$ = new Subject<void>();
   sortBtnClick$ = new Subject<void>();;
 
-
-  // TODO tmp remove
-  orderValueChanges$ = this.orderCtrl.valueChanges;
-  // TODO tmp remove
-  sortByValueChanges$ = this.sortByCtrl.valueChanges;
-
-
   // TODO 1:
   // + every time updateBtn clicked
   // + should ONCE emit current rates for: FxCurrency.Usd (use: forexApiService)
-  rates$ = this.updateBtnClick$.pipe(
+  private rates$ = this.updateBtnClick$.pipe(
     switchMap(() => this.forexApiService.getCurrentRatesFor(FxCurrency.Usd))
   );
 
   // TODO 2:
   //  + should start with: sortOptionsService.initialSortBy
   //  + should not emit until value changed
-  sortByValue$ = this.sortByCtrl.valueChanges.pipe(
+  private sortByValue$ = this.sortByCtrl.valueChanges.pipe(
     startWith(this.sortOptionsService.initialSortBy),
     distinctUntilChanged()
   );
@@ -58,7 +51,7 @@ export class LazyForexPageComponent implements OnInit {
   // TODO 3:
   //  + should start with: sortOptionsService.initialOrder
   //  + should not emit until value changed
-  orderValue$ = this.orderCtrl.valueChanges.pipe(
+  private orderValue$ = this.orderCtrl.valueChanges.pipe(
     startWith(this.sortOptionsService.initialOrder),
     distinctUntilChanged()
   );
@@ -66,7 +59,7 @@ export class LazyForexPageComponent implements OnInit {
   // TODO 4:
   // + every time sortBtn clicked
   // + should emit array: [currentSortBy, currentOrder]
-  sortInfo$ = this.sortBtnClick$.pipe(
+  private sortInfo$ = this.sortBtnClick$.pipe(
     startWith(123),
     withLatestFrom(this.sortByValue$, this.orderValue$),
     map(([_, sortByValue, orderValue]) => [sortByValue, orderValue])
