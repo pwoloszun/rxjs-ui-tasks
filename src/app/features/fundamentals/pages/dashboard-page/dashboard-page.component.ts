381af5c3ec645c2170f2af5e91b823695964df6a
pwoloszun <piotr.woloszun@gmail.com>
2022-12-01 12:29:53 +0100

    declarative subscrioptions

--- src/app/features/fundamentals/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/features/fundamentals/pages/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { PersonService } from '@features/fundamentals/services/person.service';
-import { finalize, Subject, Subscription, takeUntil } from 'rxjs';
+import { finalize, interval, map, share, shareReplay, Subject, Subscription, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -9,40 +9,17 @@ import { finalize, Subject, Subscription, takeUntil } from 'rxjs';
 })
 export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  // private completeStreams$ = new Subject<void>();
   person$ = this.personService.getPerson$().pipe(
-    // takeUntil(this.completeStreams$),
+    // share()
+    shareReplay()
   );
-
-  personData: any;
-
-  // private subscriptions: Subscription[] = [];
-  private subCont = new Subscription();
-
 
   constructor(private personService: PersonService) { }
 
   ngOnInit(): void {
-    const sub = this.person$.subscribe({
-      next: (data) => {
-        this.personData = data;
-      },
-      complete: () => {
-
-      }
-    });
-
-    // this.subscriptions.push(sub);
-
-    this.subCont.add(sub)
-
   }
 
   ngOnDestroy(): void {
-    // this.completeStreams$.next();
-
-    // this.subscriptions.forEach((s) => s.unsubscribe());
-    this.subCont.unsubscribe();
   }
 
 }
