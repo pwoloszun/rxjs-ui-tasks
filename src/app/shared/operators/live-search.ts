93012754579d6628d1257c99392e475dbd491ef9
pwoloszun <piotr.woloszun@gmail.com>
2023-01-12 11:26:59 +0100

    live search custom operator impl

--- src/app/shared/operators/live-search.ts
+++ src/app/shared/operators/live-search.ts
@@ -1,11 +1,16 @@
-import { map, Observable, OperatorFunction } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, Observable, OperatorFunction, switchMap } from 'rxjs';
 
 type DataProducerFn<T> = (query: string) => Observable<T>;
+
+const MIN_SEARCH_QUERY_LENGTH = 3;
 
 export function liveSearch<T>(time: number, producer: DataProducerFn<T>): OperatorFunction<string, T> {
   return function (source$) {
     return source$.pipe(
-      map(() => 123 as T) // TODO
+      debounceTime(time),
+      filter((query) => query.length >= MIN_SEARCH_QUERY_LENGTH),
+      distinctUntilChanged(),
+      switchMap(producer)
     );
   };
 }
