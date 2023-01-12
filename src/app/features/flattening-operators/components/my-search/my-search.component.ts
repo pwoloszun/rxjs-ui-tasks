3822417dbf88988ceea23bfee23de002da3f0f78
pwoloszun <piotr.woloszun@gmail.com>
2023-01-12 11:15:23 +0100

    live search refactor

--- src/app/features/flattening-operators/components/my-search/my-search.component.ts
+++ src/app/features/flattening-operators/components/my-search/my-search.component.ts
@@ -4,6 +4,7 @@ import { debounceTime, of } from 'rxjs';
 import { distinctUntilChanged, filter, map, switchAll, switchMap } from 'rxjs/operators';
 
 import { SearchApiService } from '@api/search-api.service';
+import { liveSearch } from '@shared/operators/live-search';
 
 const MIN_SEARCH_QUERY_LENGTH = 3;
 
@@ -32,6 +33,10 @@ export class MySearchComponent {
     filter((query) => query.length >= MIN_SEARCH_QUERY_LENGTH),
     distinctUntilChanged(),
     switchMap((query) => this.searchApiService.querySearch$(query))
+  );
+
+  searchResults_REFACTORED$ = this.searchTextCtrl.valueChanges.pipe(
+    liveSearch(800, (query) => this.searchApiService.querySearch$(query))
   );
 
   constructor(private searchApiService: SearchApiService) { }
--- src/app/shared/operators/reverse-string.ts
+++ src/app/shared/operators/reverse-string.ts
@@ -16,3 +16,26 @@ export function reverseString_LONG(): OperatorFunction<string, string> {
     );
   };
 }
+
+
+// interval(1000).pipe(
+//   // reverseString_LONG(),
+//   myMap((n: number) => n ** 2)
+// );
+
+
+// my map example
+function myMap(mappingFn: Function): OperatorFunction<any, any> {
+  return function (source$) {
+    return new Observable((obs) => {
+      source$.subscribe({
+        next(value) {
+          const mapped = mappingFn(value);
+          obs.next(mapped);
+        }
+      });
+    });
+  };
+}
+
+
