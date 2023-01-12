5c4852d6a8ef6126f7f8a4632998900bd8ced616
pwoloszun <piotr.woloszun@gmail.com>
2023-01-12 13:22:24 +0100

    lazy fx impl

--- src/app/features/combine-streams/pages/lazy-forex-page/lazy-forex-page.component.ts
+++ src/app/features/combine-streams/pages/lazy-forex-page/lazy-forex-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { map, of, Subject, switchMap, distinctUntilChanged, startWith } from 'rxjs';
+import { map, of, Subject, switchMap, distinctUntilChanged, startWith, combineLatest } from 'rxjs';
 import { orderBy } from 'lodash';
 
 import { ForexApiService, FxCurrency } from '@api/forex-api.service';
 
 import { ForexSortOptionsService } from '../../services/forex-sort-options.service';
-import { withLatestFrom } from 'rxjs/operators';
+import { combineLatestWith, withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lazy-forex-page',
@@ -32,23 +32,17 @@ export class LazyForexPageComponent implements OnInit {
   updateBtnClick$ = new Subject<void>();
   sortBtnClick$ = new Subject<void>();;
 
-  // TODO tmp remove
-  orderValueChanges$ = this.orderCtrl.valueChanges;
-  // TODO tmp remove
-  sortByValueChanges$ = this.sortByCtrl.valueChanges;
-
-
   // TODO 1:
   // + every time updateBtn clicked
   // + should ONCE emit current rates for: FxCurrency.Usd (use: forexApiService)
-  rates$ = this.updateBtnClick$.pipe(
+  private rates$ = this.updateBtnClick$.pipe(
     switchMap(() => this.forexApiService.getCurrentRatesFor(FxCurrency.Usd)),
   );
 
   // TODO 2:
   //  + should start with: sortOptionsService.initialSortBy
   //  + should not emit until value changed
-  sortByValue$ = this.sortByCtrl.valueChanges.pipe(
+  private sortByValue$ = this.sortByCtrl.valueChanges.pipe(
     startWith(this.sortOptionsService.initialSortBy),
     distinctUntilChanged()
   );
@@ -56,7 +50,7 @@ export class LazyForexPageComponent implements OnInit {
   // TODO 3:
   //  + should start with: sortOptionsService.initialOrder
   //  + should not emit until value changed
-  orderValue$ = this.orderCtrl.valueChanges.pipe(
+  private orderValue$ = this.orderCtrl.valueChanges.pipe(
     startWith(this.sortOptionsService.initialOrder),
     distinctUntilChanged()
   );
@@ -64,7 +58,7 @@ export class LazyForexPageComponent implements OnInit {
   // TODO 4:
   // + every time sortBtn clicked
   // + should emit array: [currentSortBy, currentOrder]
-  sortInfo$ = this.sortBtnClick$.pipe(
+  private sortInfo$ = this.sortBtnClick$.pipe(
     startWith(undefined),
     withLatestFrom(this.sortByValue$, this.orderValue$),
     map(([_, sortBy, order]) => [sortBy, order])
@@ -74,7 +68,14 @@ export class LazyForexPageComponent implements OnInit {
   // + should emit every time either rates or full sortInfo changes
   // + should order rates based on full sortInfo (sortBy, order)
   // see: orderBy function on https://lodash.com/docs
-  sortedRatesChanges$ = of([]);
+  sortedRatesChanges$ = combineLatest([
+    this.rates$,
+    this.sortInfo$
+  ]).pipe(
+    map(([rates, [sortBy, order]]) => {
+      return orderBy(rates, [sortBy], [order as any]);
+    })
+  );
 
   // orderBy(ratings, ['updatedAt'], ['desc'])
 
